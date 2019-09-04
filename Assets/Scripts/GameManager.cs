52af3342f55d58b72b687be80f92c26dfe3edc38
Rafał Dzwonek <rafal.dzwonek@o2.pl>
2019-09-04 17:41:40 +0200

    Mulit
    Niby działa ale jednak nie :(

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    //skrypt odpoiedzialny za grę
+
+    bool hasEnded = false;
+
+    private void Update()
+    {
+        
+    }
+
+    public void TheEndgame()
+    {
+        if (hasEnded)
+        {
+            return;
+        }
+
+        // dodać txxtboxy!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        Debug.Log("ZDECHŁEŚ");
+        hasEnded = true;
+        StartCoroutine(PlayEndgame());
+    }
+
+    IEnumerator PlayEndgame()
+    {
+        Debug.Log("KONIEC GRY");
+
+        //po sekundzie zrestartuj gre
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Movement : NetworkBehaviour
+{
+    public float speed = 0f;
+    public float rotationSpeed = 200f;
+    public KeyCode buttonLeft = KeyCode.A;
+    public KeyCode buttonRight = KeyCode.D;
+    bool ready = false;
+
+    float horizontal = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(buttonLeft))
+        {
+            horizontal = -1;
+        }
+        else if (Input.GetKey(buttonRight))
+        {
+            horizontal = 1;
+        }
+        else
+        {
+            horizontal = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //wzor na poruszanie
+        transform.Translate(Vector2.up * speed * Time.fixedDeltaTime, Space.Self);
+        //obrót
+        transform.Rotate(Vector3.forward*-horizontal*rotationSpeed*Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //rzuć texboxa do tej formuły !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        if (col.tag == "killer")
+        {
+            GameManager.FindObjectOfType<GameManager>().TheEndgame();
+            speed = 0f;
+            rotationSpeed = 0f;
+        }
+    }
+}
